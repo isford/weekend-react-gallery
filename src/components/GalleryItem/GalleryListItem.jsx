a2bd5bf45c6d0a07de71bba0a48aa7fe5369366d
Ian <isford7@gmail.com>
2021-06-05 13:00:37 -0500

    Description toggle working

--- src/components/GalleryItem/GalleryListItem.jsx
+++ src/components/GalleryItem/GalleryListItem.jsx
@@ -1,10 +1,10 @@
 import './GalleryListItem.css'
-import {useState} from 'react';
+import { useState } from 'react';
 import axios from 'axios'
 
-function GalleryListItem ({item, getGalleryList}){
-    let [liked, setLiked] = useState(0);
-
+function GalleryListItem({ item, getGalleryList }) {
+    const [liked, setLiked] = useState(0);
+    const [isHidden, setIsHidden] = useState(true);
 
     const handleLike = () => {
         console.log('This was liked', item);
@@ -13,22 +13,37 @@ function GalleryListItem ({item, getGalleryList}){
         }
 
         //PUT for LIKE
-        axios.put(`/gallery/${item.id}`, data).then(response =>{
+        axios.put(`/gallery/${item.id}`, data).then(response => {
             console.log(response);
             //item.likes + 1;
             setLiked = data.isLiked;
             getGalleryList();
         }).catch(err => {
             console.log(err)
-        })}
+        })
+    }
+
+
+
+    // const handleToggleDescription = () => {
+    //     { isHidden ? (<div>{item.description}</div>) : (<div></div>)}
+    //     }
+
+
     return (
-        <div className = "listItem">
-            <div>
-                {item.description}
-                <img src={item.path}/>
-            </div>
-        <button onClick ={handleLike}>Like</button>
-        <>Likes:{liked} </>
+        <div className="listItem">
+            { isHidden ? (
+                <div>
+                    <img src={item.path} />
+                </div>
+            ) : (
+                <div>
+                    {item.description}
+                </div>
+            )}
+            <button onClick={() => setIsHidden(!isHidden)} >{!isHidden ? 'Show Photo' : 'Show Description'}</button>
+            <button onClick={handleLike}>Like</button>
+            <>Likes:{liked} </>
         </div>
     )
 }
